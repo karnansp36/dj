f70338878f3f2a8f587797ba4ce64cddd0564ee8
karnansp36 <reviewmaster36@gmail.com>
2026-01-23 12:01:07 +0530

    updated

--- post/views.py
+++ post/views.py
@@ -17,4 +17,33 @@ def signup(request):
     return render(request, "signup.html")
 
 def home(request):
-    return render(request, "index.html")
+    post = Post.objects.all()
+    post2 = [{"title":"john", "description": "this is django"} ,
+            {"title":"peter", "description": "this is django"} ,
+            {"title":"john", "description": "this is django"} ]
+    return render(request, "index.html", {"posts":post})
+
+def detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "post.html", {"post":post})
+
+def delete_post(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return HttpResponse("Post deleted successfully!")
+
+def update_post(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        email = request.POST.get("email")
+
+        post.title = title
+        post.description = description  
+        post.email = email
+        post.save()
+        
+        return HttpResponse("Post update successfully!")
+    return render(request, "signup.html", {"post": post})
